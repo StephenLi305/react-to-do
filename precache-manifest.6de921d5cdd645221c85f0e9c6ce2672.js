0a117e71fd6526f6cf6cd479350ee5609f708dac
Stephen Li <stephen.li305@gmail.com>
2019-08-08 10:33:35 -0700

    Updates

---
+++ precache-manifest.6de921d5cdd645221c85f0e9c6ce2672.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "974169d36b24c18bbc6d7a93cb75fff9",
+    "url": "/react-to-do/index.html"
+  },
+  {
+    "revision": "ff22648a311f309fe0c9",
+    "url": "/react-to-do/static/css/main.454d6f9b.chunk.css"
+  },
+  {
+    "revision": "5c9ff16ec5bee154aee2",
+    "url": "/react-to-do/static/js/2.5286435d.chunk.js"
+  },
+  {
+    "revision": "ff22648a311f309fe0c9",
+    "url": "/react-to-do/static/js/main.7ba3997c.chunk.js"
+  },
+  {
+    "revision": "26b717aa96a48dc90608",
+    "url": "/react-to-do/static/js/runtime~main.8def430e.js"
+  }
+]);
