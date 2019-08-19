c71229c4c600e9a8033969d0035d82d629257730
Stephen Li <stephen.li305@gmail.com>
2019-08-19 14:32:55 -0700

    added AL icon

--- src/Footer.js
+++ src/Footer.js
@@ -7,6 +7,7 @@ const Footer = () => {
             <a href="https://github.com/StephenLi305" target="_blank" rel="noopener noreferrer"><i className="fab fa-github"></i></a>
             <a href="https://www.linkedin.com/in/stephenli305/" target="_blank" rel="noopener noreferrer"><i className="fab fa-linkedin-in"></i></a>
             <a href="https://stephenli.fun/" target="_blank" rel="noopener noreferrer"><i className="fas fa-portrait"></i></a>
+            <a href="https://angel.co/stephen-li-15" target="_blank" rel="noopener noreferrer"><i className="fab fa-angellist"></i></a>
         </div>
     )
 
